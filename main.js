a5feaa6d40a6d716c8c3c46909c4a476a75a1387
BLxcwg666 <huixcwg@gmail.com>
2023-10-21 16:43:31 +0800

    [1.2] 添加控制台用户发送日志

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const TelegramBot = require("node-telegram-bot-api");
 const dotenv = require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
+const chalk = require("chalk");
 
 const db = new sqlite3.Database('sqlite.db');
 db.run("CREATE TABLE IF NOT EXISTS files (ID INTEGER PRIMARY KEY AUTOINCREMENT, UserID INTEGER, OriginLink TEXT, FileName TEXT, Time DATETIME)");
@@ -44,32 +45,43 @@ const { getFileLink } = require('./fetcher');
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  const currentTime = new Date().toISOString().slice(0, 19).replace('T', ' ');
 
   if (msg.document) {
     const fileId = msg.document.file_id;
     const UserID = msg.from.id;
     const FileName = msg.document.file_name;
     getFileLink(bot, db, chatId, fileId, UserID, FileName);
+    console.log(chalk.green(`[${currentTime}] ${UserID} Sent ${FileName}`));
+
   } else if (msg.audio) {
     const fileId = msg.audio.file_id;
     const UserID = msg.from.id;
     const FileName = msg.audio.file_name;
     getFileLink(bot, db, chatId, fileId, UserID, FileName);
+    console.log(chalk.green(`[${currentTime}] ${UserID} Sent ${FileName}`));
+
   } else if (msg.video) {
     const fileId = msg.video.file_id;
     const UserID = msg.from.id;
     const FileName = msg.video.file_name;
     getFileLink(bot, db, chatId, fileId, UserID, FileName);
+    console.log(chalk.green(`[${currentTime}] ${UserID} Sent ${FileName}`));
+
   } else if (msg.photo) {
     const fileId = msg.photo[msg.photo.length - 1].file_id;
     const UserID = msg.from.id;
     const FileName = `photo_${msg.message_id}.jpg`;
     getFileLink(bot, db, chatId, fileId, UserID, FileName);
+    console.log(chalk.green(`[${currentTime}] ${UserID} Sent ${FileName}`));
+
   } else if (msg.sticker) {
     const fileId = msg.sticker.file_id;
     const UserID = msg.from.id;
     const FileName = `sticker_${msg.message_id}.webp`;
     getFileLink(bot, db, chatId, fileId, UserID, FileName);
+    console.log(chalk.green(`[${currentTime}] ${UserID} Sent ${FileName}`));
+
   } else {
     bot.sendMessage(chatId, "无效的文件类型");
   }
